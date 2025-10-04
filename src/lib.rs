5ecaa5de67592ed94664ac9d9af23cf9d6c4c028
POTI <potistudio@proton.me>
2025-10-05 01:26:52 +0900

    chore: Add example to crate doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,45 @@
-//! A Rust implementation of UUIDv47 that converts between UUIDv7 and UUIDv4.
+//! A simple and lightweight library for converting between UUIDv7 and UUIDv4 facade.
+//! This is a Rust implementation of [UUIDv47](https://github.com/stateless-me/uuidv47).
+//!
+//! # Features
+//!
+//! - **Safe**: Implemented all APIs in safe Rust.
+//! - **Easy**: Provides simple object-oriented API.
+//! - **Fast**: Designed to eliminate overhead and run at high performance.
+//! - **Lightweight** Zero dependencies.
+//!
+//! # Example
+//!
+//! ```
+//! use uuid47::*;
+//!
+//! fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let s = "00000000-0000-7000-8000-000000000000";  // your UUIDv7 string
+//!     let key = UuidV47Key::new(0x0123456789abcdef, 0xfedcba9876543210);  // your 128-bit key
+//!
+//!     // Parse str to UUIDv7
+//!     // error if provided string is invalid
+//!     let v7: Uuid128 = s.parse()?;
+//!     println!("v7(DB)  : {}", v7);
+//!
+//!     // Encode UUIDv7 to UUIDv4 facade
+//!     let facade = v7.encode_v7(&key);
+//!     println!("v4(API) : {}", facade);
+//!
+//!     // Decode UUIDv4 facade to UUIDv7
+//!     let back = facade.decode_v4facade(&key);
+//!     println!("back    : {}", back);
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! ```
+//! // Output:
+//! // 7(DB)   : 00000000-0000-7000-8000-000000000000
+//! // 4(API)  : 22d97126-9609-4000-8000-000000000000
+//! // back    : 00000000-0000-7000-8000-000000000000
+//! ```
 
 #![deny(missing_docs)]
 #![warn(clippy::all)]
